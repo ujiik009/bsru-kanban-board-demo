94d684c421adb49483f4cb3719fec1d57dbd1385
LDC.ICE <ikool009@hotmail.com>
2021-11-19 22:41:59 +0700

    finish Member Component

---
+++ src/components/ProjectContent/components/Member.vue
@@ -0,0 +1,189 @@
+<template>
+  <div id="view-content">
+    <div class="header">
+      <div class="board-label">{{ project.name }} Kanban Board</div>
+      <div>
+        <b-button class="btn-primary" @click="open_invite_member_modal = true"
+          >Invite member</b-button
+        >
+      </div>
+    </div>
+    <div>
+      <b-row class="box">
+        <b-col
+          style="text-align: center"
+          sm="12"
+          md="3"
+          v-for="(member_item, index) in members"
+          :key="index"
+        >
+          <center>
+            <div class="member-item">
+              <b-avatar :src="member_item.image_profile" size="9rem"></b-avatar>
+              <div class="fullname">
+                {{ member_item.fullname }}
+              </div>
+              <div class="position">
+                {{ member_item.position }}
+              </div>
+            </div>
+          </center>
+        </b-col>
+      </b-row>
+    </div>
+    <!-- modal open invite_member -->
+
+    <div>
+      <b-modal
+        v-if="open_invite_member_modal == true"
+        v-model="open_invite_member_modal"
+        title="Invite Members to you project"
+        hide-footer
+        size="md"
+      >
+        <div class="my-4">
+          <b-form-group
+            label="Email Address"
+            label-for="input-1"
+            style="margin-bottom: 10px"
+            v-for="(invite_member_item, index) in invite_member"
+            :key="index"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="invite_member_item.email"
+              type="text"
+              placeholder="Enter you eamil address"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <span @click="add_new" class="add-new">Add New</span>
+          <div style="text-align: right">
+            <b-button style="margin-right: 10px">Cancel</b-button>
+            <b-button class="btn-primary">Update</b-button>
+          </div>
+        </div>
+      </b-modal>
+    </div>
+    <!-- modal open invite_member -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project_id: String,
+    project: Object,
+  },
+  methods: {
+    add_new() {
+      this.invite_member.push({
+        email: "",
+      });
+    },
+  },
+  data() {
+    return {
+      invite_member: [
+        {
+          email: "",
+        },
+        {
+          email: "",
+        },
+      ],
+      open_invite_member_modal: false,
+      members: [
+        {
+          id: 1,
+          fullname: "Jennifer S. Byrd",
+          position: "Backend Developer",
+          image_profile:
+            "https://static01.nyt.com/newsgraphics/2020/11/12/fake-people/4b806cf591a8a76adfc88d19e90c8c634345bf3d/fallbacks/mobile-05.jpg",
+        },
+        {
+          id: 2,
+          fullname: "Thomas J. Kohut",
+          position: "UX/UI Designer",
+          image_profile:
+            "https://static01.nyt.com/newsgraphics/2020/11/12/fake-people/4b806cf591a8a76adfc88d19e90c8c634345bf3d/fallbacks/mobile-05.jpg",
+        },
+        {
+          id: 3,
+          fullname: "Kenneth S. Ahrens",
+          position: "Frontend Developer",
+          image_profile:
+            "https://static01.nyt.com/newsgraphics/2020/11/12/fake-people/4b806cf591a8a76adfc88d19e90c8c634345bf3d/fallbacks/mobile-05.jpg",
+        },
+        {
+          id: 4,
+          fullname: "Jason S. Footman",
+          position: "Mobile Application Developer",
+          image_profile:
+            "https://thecreativemind.net/wp-content/uploads/2015/04/Rose-Byrne.jpg",
+        },
+        {
+          id: 5,
+          fullname: "Jonathan R. Bartlett",
+          position: "Product Manager",
+          image_profile:
+            "https://thecreativemind.net/wp-content/uploads/2015/04/Rose-Byrne.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.box {
+  padding-left: 200px;
+  padding-right: 200px;
+}
+.member-item {
+  border-radius: 10px;
+  margin: 10px;
+  min-height: 300px;
+  width: 250px;
+  text-align: center;
+  padding: 20px;
+  -webkit-box-shadow: 0px 0px 5px 0px rgba(97, 97, 97, 1);
+  -moz-box-shadow: 0px 0px 5px 0px rgba(97, 97, 97, 1);
+  box-shadow: 0px 0px 5px 0px rgba(97, 97, 97, 1);
+}
+#view-content {
+  padding: 20px;
+  height: calc(100% - 160px);
+
+  overflow-y: auto;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+.board-label {
+  color: #43435e;
+  font-size: 30px;
+  font-weight: bold;
+}
+.btn-primary {
+  background-color: #7741a0;
+}
+.fullname {
+  margin-top: 50px;
+  color: #43435e;
+  font-size: 23px;
+  font-weight: bold;
+}
+.position {
+  color: #80808d;
+  font-size: 15px;
+}
+
+.add-new {
+  cursor: pointer;
+  color: #4fa0ff;
+  font-size: 17px;
+}
+</style>
--- src/components/ProjectContent/index.vue
+++ src/components/ProjectContent/index.vue
@@ -4,11 +4,13 @@
       <b-tabs content-class="mt-3">
         <b-tab title="Kanban Board" active>
           <div>
-            <KanbanBoard />
+            <KanbanBoard :project="project" :project_id="project_id" />
           </div>
         </b-tab>
         <b-tab title="Members">
-          <div>Member Content</div>
+          <div>
+            <Member :project="project" :project_id="project_id" />
+          </div>
         </b-tab>
       </b-tabs>
     </div>
@@ -17,22 +19,35 @@
 
 <script>
 import KanbanBoard from "@/components/ProjectContent/components/KanbanBoard.vue";
+import Member from "@/components/ProjectContent/components/Member.vue";
 export default {
   components: {
     KanbanBoard,
+    Member,
+  },
+  data() {
+    return {
+      project_id: this.$route.params.project_id,
+      project: {
+        id: 1,
+        name: "Web Desinger Project",
+        percent: 50,
+        task_count: 50,
+        duration: "2020-12-30 00:00:00",
+        created_at: "2020-11-01 00:00:00",
+      },
+    };
   },
 };
 </script>
 
 <style>
-
 .content-page {
   height: 100%;
   overflow-y: auto;
   background-color: #e7e7e7;
   padding-left: 20px;
   padding-right: 20px;
- 
 }
 .content-body {
   margin: 20px;
